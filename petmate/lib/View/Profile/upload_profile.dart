24e16112075f0a65e239d6ec1e31f53368aa3854
ohjeonghyun <111102264+jeonghyun77@users.noreply.github.com>
2024-07-01 22:28:57 +0900

    geolocator

--- petmate/lib/View/Profile/upload_profile.dart
+++ petmate/lib/View/Profile/upload_profile.dart
@@ -1,23 +1,14 @@
-import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
-import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:petmate/Util/textstyles.dart';
-import 'package:petmate/View/Notice/noticedelete.dart';
-import 'package:petmate/View/mainpage.dart';
 import 'package:petmate/Widget/bottom_navigation_bar/bottom_navigationbar.dart';
 import 'package:petmate/Widget/custom_appbar.dart';
-import 'package:petmate/Widget/notice/notification_container.dart';
-import 'package:petmate/Widget/notice/notification_type.dart';
 import 'package:petmate/Widget/profile/add/add_info.dart';
 import 'package:petmate/Widget/profile/add/add_name.dart';
 import 'package:petmate/Widget/profile/add/add_profile.dart';
-import 'package:petmate/Widget/profile/deleted/mypet_deleted_widget.dart';
-import 'package:petmate/Widget/profile/deleted/mypet_edit_widget.dart';
 
 class UploadProfilePage extends StatefulWidget {
   const UploadProfilePage({super.key});
@@ -31,9 +22,7 @@ class _UploadProfilePage extends State<UploadProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      backgroundColor: Colors.transparent,
-      extendBodyBehindAppBar: true,
-      appBar: CustomAppbar(title: '등록'),
+      extendBodyBehindAppBar: false,
       body: Container(
         width: 360.w,
         height: 800.h,
@@ -42,27 +31,38 @@ class _UploadProfilePage extends State<UploadProfilePage> {
               image: AssetImage('assets/Main/Background.png'),
               fit: BoxFit.fill),
         ),
-        child: Stack(
-          children: [
-            Positioned(top: 102.h, left: 132.w, child: AddProfileWidget()),
-            Positioned(top: 218.h, left: 55.w, child: AddName()),
-            // Positioned(top: 302.h, left: 8.w, child: AddInfoWidget()),
-            Positioned(
-              top: 302.h,
-              left: 8.w,
-              right: 8.w,
-              bottom: 0,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    AddInfoWidget(),
-                    SizedBox(
-                      height: 70,
-                    )
-                  ],
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              scrolledUnderElevation: 0,
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.transparent,
+              centerTitle: true,
+              leading: GestureDetector(
+                onTap: () {
+                  Get.back();
+                },
+                child: Image.asset(
+                  'assets/onboarding/icon_5.png',
                 ),
               ),
+              title: Text(
+                '등록',
+                style: White(20.sp, FontWeight.w600),
+              ),
             ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  AddProfileWidget(),
+                  AddName(),
+                  AddInfoWidget(),
+                  SizedBox(
+                    height: 75,
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
